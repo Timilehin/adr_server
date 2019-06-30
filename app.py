3608ba3038dca7fda92af6ce0c4b4aad959991ce
Timi Fasubaa <timifasubaa@gmail.com>
2019-06-30 06:51:48 -0700

    move webapges to templates

--- app.py
+++ app.py
@@ -8,14 +8,8 @@ YORUBA_TTS_LINK_BASE = "http://ttsyoruba.com"
 
 
 @app.route("/")
-def hello():
-    form = """<h1> Welome to the Yoruba Diacritizer</h1> <br>
-      <form action="/predict" method="POST">
-        Undiacritized Yoruba:<br>
-        <input type="text" name="text"><br>
-        <input type="submit" value="Submit">
-      </form>"""
-    return form
+def index():
+    render_template('index.html')
 
 
 @app.route("/predict", methods=['GET', 'POST'])
@@ -23,7 +17,8 @@ def predict():
     user_input = flask.request.form.get("text")
     result = iranlowo.adr.diacritize_text(user_input)
     yoruba_tts_link = YORUBA_TTS_LINK_BASE + "/?q="+result
-    return "<h1> Welome to the Yoruba Diacritizer</h1> <br> Undiacritized: " + user_input + "<br> <br> Diacritized: " + result + "<br> <br> <a href='" + yoruba_tts_link + "'>Hear it </a> <br> <br> <a href='/'>Diacritize more</a>"
+    render_template('result.html', user_input=user_input, result=result, yoruba_tts_link=yoruba_tts_link)
+    #return ""
     
 if __name__ == '__main__':
     print("Flask starting server ...")
